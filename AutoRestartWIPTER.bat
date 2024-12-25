bd82cd2c454209cb0333f99f2a28285ed23828eb
trungdungalpha <trungdungalpha@gmail.com>
2024-12-25 10:04:00 +0700

    Update AutoRestartWIPTER.bat

--- AutoRestartWIPTER.bat
+++ AutoRestartWIPTER.bat
@@ -16,9 +16,14 @@ REM Additional commands at the start
 TASKKILL /F /IM explorer.exe
 start "explorer.exe" "C:\Windows\explorer.exe"
 timeout 3
-TASKKILL /F /IM shutdown.exe
-taskkill /F /IM Wipter.exe
-timeout /t 30
+
+REM Count the total number of files in the folder (excluding subfolders)
+set fileCount=0
+for /f "delims=" %%F in ('dir "%folder%" /a-d /b') do (
+    set /a fileCount+=1
+)
+
+echo Total number of files in the folder: %fileCount%
 
 REM Loop through files in alphabetical order
 for /f "delims=" %%F in ('dir "%folder%" /a-d /b') do (
@@ -39,6 +44,17 @@ set /a hours=%random% %% 2 + 4
 set /a seconds=hours * 3600
 echo Waiting for a random time of %hours% hours (%seconds% seconds)...
 timeout /t %seconds% >nul
+
+REM Kill all .exe processes from 1 to fileCount (based on file count)
+for /l %%i in (1,1,%fileCount%) do (
+    REM Construct the filename for the process (assuming the filename is the same as the process name)
+    REM This assumes the files are named as `1.exe`, `2.exe`, ..., `n.exe`
+    taskkill /F /IM %%i.exe
+    echo Killed process %%i.exe
+)
+TASKKILL /F /IM shutdown.exe
+taskkill /F /IM Wipter.exe
+timeout /t 30
 
 set /a loopCount=%loopCount%-1
 if %loopCount%==0 GOTO:EOF
